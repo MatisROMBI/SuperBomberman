b011557dfeb8252cbc04f0d8f25e8f7b01f61e02
kirito.016 <imen.ben-gadha@etu.univ-amu.fr>
2025-06-06 01:22:21 +0200

    changement du menu principal

--- src/main/java/com/bomberman/controller/MainMenuController.java
+++ src/main/java/com/bomberman/controller/MainMenuController.java
@@ -3,16 +3,28 @@ package com.bomberman.controller;
 import com.bomberman.utils.SceneManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainMenuController {
     @FXML
     private Button playButton;
-
     @FXML
     private Button quitButton;
+    @FXML
+    private ImageView backgroundImage;
 
     @FXML
     private void initialize() {
+        try {
+            // Remplace "menu_bg.png" par le vrai nom de ton image (attention à la casse et aux espaces !)
+            Image img = new Image(getClass().getResourceAsStream("/images/menu_bg.png"));
+            backgroundImage.setImage(img);
+        } catch (Exception e) {
+            backgroundImage.setImage(null);
+            System.err.println("Erreur chargement image fond menu : " + e.getMessage());
+        }
+
         playButton.setOnAction(e -> startGame());
         quitButton.setOnAction(e -> System.exit(0));
     }
